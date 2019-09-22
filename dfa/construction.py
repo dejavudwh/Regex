99648752c8ea3e9de461a0afe34def0486a0fd9e
dejavudwh <494319000@qq.com>
2019-09-22 10:23:34 +0800

    Complete the transformation from NFA to DFA

--- dfa/construction.py
+++ dfa/construction.py
@@ -13,18 +13,25 @@ from utils import list_dict
 def convert_to_dfa(nfa_start_node):
     jump_table = list_dict(MAX_DFA_STATUS_NUM)
     dfa_list = []
-    n_closure = closure(nfa_start_node)
+    ns = [nfa_start_node]
+    n_closure = closure(ns)
     dfa = Dfa.nfas_to_dfa(n_closure)
     dfa_list.append(dfa)
 
     dfa_index = 0
     while dfa_index < len(dfa_list):
+        # print('dfalist: index, ********', dfa_list, dfa_index)
         dfa = dfa_list[dfa_index]
         for i in range(ASCII_COUNT):
             c = chr(i)
             nfa_move = move(dfa.nfa_sets, c)
+            # print('nfa move: ********', nfa_move)
             if nfa_move is not None:
                 nfa_closure = closure(nfa_move)
+                # print('nfa closure: ********', nfa_closure)
+                if nfa_closure is None:
+                    # print('fffua')
+                    continue
                 new_dfa = convert_completed(dfa_list, nfa_closure)
                 if new_dfa is None:
                     new_dfa = Dfa.nfas_to_dfa(nfa_closure)
@@ -36,10 +43,12 @@ def convert_to_dfa(nfa_start_node):
     return jump_table
     
 
-
 def convert_completed(dfa_list, closure):
     for dfa in dfa_list:
         if dfa.nfa_sets == closure:
             return dfa
 
     return None
+
+
+# def log_dfa(start_node):
--- dfa/dfa.py
+++ dfa/dfa.py
@@ -5,16 +5,19 @@ class Dfa(object):
     STATUS_NUM = 0
 
     def __init__(self):
-        self.nfa_sets = set()
+        self.nfa_sets = []
         self.accepted = False
         self.status_num = -1
 
     @classmethod
     def nfas_to_dfa(cls, nfas):
+        print('come in to**')
         dfa = cls()
         for n in nfas:
             dfa.nfa_sets.append(n)
+            print('nfa status num **********', n.status_num)
             if n.next_1 is None and n.next_2 is None:
+                print('is accepted ************* ', n.status_num)
                 dfa.accepted = True
 
         dfa.status_num = Dfa.STATUS_NUM
